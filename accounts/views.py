3bc69f1ec20bcff437db75f6160b3e1bbc2f271f
smart0120 <gadrgon0305@gmail.com>
2020-12-10 16:40:15 -0300

    added views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions
 
-# Create your views here.
+from . import serializers
+
+CustomUser = get_user_model()
+
+
+class UserRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    **GET:** List details for a ``CustomUser``.
+    **PUT:** Update details of a ``CustomUser``.
+    **DELETE:** Delete a specific ``CustomUser``.
+    This view can be used to retrieve data for the current logged in user.
+    """
+
+    queryset = CustomUser.objects.all()
+    serializer_class = serializers.CustomUserRetrieveSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
