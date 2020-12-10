fdab2f9f5b2137a1391fb8fcd3c18306f5294236
smart0120 <gadrgon0305@gmail.com>
2020-12-10 16:38:46 -0300

    added serializers.py

---
+++ accounts/serializers.py
@@ -0,0 +1,44 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers, validators
+
+CustomUser = get_user_model()
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    """
+    We use this serializer for user registration. Most of the fields have
+    `required=False`, but can be configured as needed. This serializer is used
+    in `accounts.viewsets.CustomUserModelViewSet`.
+    """
+
+    email = serializers.CharField(
+        write_only=True, validators=[validators.UniqueValidator(
+            message='This email already exists',
+            queryset=CustomUser.objects.all()
+        )]
+    )
+    password = serializers.CharField(write_only=True)
+    birth_date = serializers.CharField(required=False)
+    gender = serializers.CharField(required=False)
+    last_name = serializers.CharField(required=False)
+    first_name = serializers.CharField(required=False)
+    birth_date = serializers.CharField(required=False)
+
+    class Meta:
+        model = CustomUser
+        fields = ('first_name', 'last_name', 'email',
+                  'password', 'gender', 'birth_date')
+
+
+class CustomUserRetrieveSerializer(serializers.ModelSerializer):
+    """
+    We use this serializer to retrieve data of the currently logged in user.
+    It is used in `accounts.views.UserRetrieveUpdateDestroyAPIView`
+    """
+    birth_date = serializers.CharField(required=False)
+    gender = serializers.CharField(required=False)
+
+    class Meta:
+        model = CustomUser
+        fields = ('first_name', 'last_name', 'email',
+                  'gender', 'birth_date', 'id')
