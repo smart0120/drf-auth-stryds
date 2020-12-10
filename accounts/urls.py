57f19f67dd1fbefd6a050ec823b52e918a3b3f74
smart0120 <gadrgon0305@gmail.com>
2020-12-10 16:41:47 -0300

    added routers and updated accounts/urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,10 @@
 from django.urls import include, path
 
+from . import routers, views
 
 urlpatterns = [
+    path('data/', views.UserRetrieveUpdateDestroyAPIView.as_view(),
+         name='user-data'),  # get data for the currently logged in user
+    path('users/', include(routers.router.urls)),  # provides a few default
+    # views that we can use for our CRUD operations.
 ]
